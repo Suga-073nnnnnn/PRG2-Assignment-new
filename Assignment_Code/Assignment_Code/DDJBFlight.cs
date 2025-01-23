ab412e7ecb0e5e084491a1f737cb3962f73cf4a7
Gabriel101025 <S10259250@connect.np.edu.sg>
2025-01-23 11:10:47 +0800

    Update DDJBFlight.cs

--- Assignment_Code/Assignment_Code/DDJBFlight.cs
+++ Assignment_Code/Assignment_Code/DDJBFlight.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Assignment_Code
 {
-    internal class DDJBFlight
+    class DDJBFlight: flight
+{
+    public double requestFee { get; set; }
+    public DDJBFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status, double requestFees) : base(flightNumber, origin, destination, expectedTime, status)
     {
+        requestFee = 300;
     }
+    public override double CalculateFee()
+    {
+        return 500 + 800 + 300 + requestFee;
+    }
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
 }
