2e586a3c496dcfd6f767a2218ec1918742b6514e
Gabriel101025 <S10259250@connect.np.edu.sg>
2025-01-23 11:12:01 +0800

    Update CFFTFlight.cs

--- Assignment_Code/Assignment_Code/CFFTFlight.cs
+++ Assignment_Code/Assignment_Code/CFFTFlight.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Assignment_Code
 {
-    internal class CFFTFlight
+    class CFFTFlight: flight
     {
+        public double requestFees {  get; set; }
+        public CFFTFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status, double requestFees):base(flightNumber,origin, destination, expectedTime, status)
+        {
+            requestFees = 150;
+        }
+        public override double CalculateFee()
+        {
+            return 500 + 800 + 300 + requestFees;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
