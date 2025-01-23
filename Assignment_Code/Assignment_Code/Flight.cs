c2213d4a6f41a9a061d9579e6b5de69f6a3cc9c8
Gabriel101025 <S10259250@connect.np.edu.sg>
2025-01-23 11:15:25 +0800

    Update Flight.cs

--- Assignment_Code/Assignment_Code/Flight.cs
+++ Assignment_Code/Assignment_Code/Flight.cs
@@ -7,25 +7,25 @@ using System.Threading.Tasks;
 namespace Assignment_Code
 {
     class flight
-{
-    public string flightNumber { get; set; }
-    public string origin { get; set; }
-    public string destination { get; set; }
-    public DateTime expectedTime { get; set; }
-    public string status { get; set; }
-    public flight(string flightNumber, string origin, string destination, DateTime expectedTime, string status)
     {
-        this.flightNumber = flightNumber;
-        this.origin = origin;
-        this.destination = destination;
-        this.expectedTime = expectedTime;
-        this.status = status;
+        public string flightNumber { get; set; }
+        public string origin { get; set; }
+        public string destination { get; set; }
+        public DateTime expectedTime { get; set; }
+        public string status { get; set; }
+        public flight(string flightNumber, string origin, string destination, DateTime expectedTime, string status)
+        {
+            this.flightNumber = flightNumber;
+            this.origin = origin;
+            this.destination = destination;
+            this.expectedTime = expectedTime;
+            this.status = status;
+        }
+        public override double CalculateFee() { };
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+        
     }
-    public override double CalculateFee() { };
-    public override string ToString()
-    {
-        return base.ToString();
-    }
-    
-}
 }
