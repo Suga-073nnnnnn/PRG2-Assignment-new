7017cae33870cf1eed3014603077fab7711b7c42
Sugaaaa <s10270022@connect.np.edu.sg>
2025-01-26 19:04:56 +0800

    Implemented the BoardingGate Class with all the attributes and methods

--- Assignment_Code/Assignment_Code/BoardingGate.cs
+++ Assignment_Code/Assignment_Code/BoardingGate.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace Assignment_Code
 {
-    internal class BoardingGate
+    class BoardingGate
     {
+        public string GateName { get; set; }
+        public bool SupportsCFFT { get; set; }
+        public bool SupportsDDJB { get; set; }
+        public bool SupportsLWT { get; set; }
+        public flight Flight { get; set; }
+
+        public BoardingGate(string gateName, bool supportsCFFT, bool supportsDDJB, bool supportsLWT)
+        {
+            GateName = gateName;
+            SupportsCFFT = supportsCFFT;
+            SupportsDDJB = supportsDDJB;
+            SupportsLWT = supportsLWT;
+            Flight = null; //not sure, might change later, temp value (remove when decided/confirmed)
+        }
+
+        public double CalculateFees()
+        {
+            if (Flight != null)     //Thinking of possibility to check if null in main before calling instead. *Need to discuss
+            {
+                return Flight.CalculateFee();
+            }
+            else
+            {
+                return 0.0;
+            }
+        }
+
+        public override string ToString()       //Should be override. *Need to confirm with Gabriel but should work as intended.
+        {
+            // Format based on the spacing in the Sample output. *Might be changed / edited later (remove when confirmed)
+            return string.Format("{0,-14}{1,-18}{2,-18}{3,-18}",
+            GateName,
+            SupportsDDJB,
+            SupportsCFFT,
+            SupportsLWT);
+        }
     }
 }
