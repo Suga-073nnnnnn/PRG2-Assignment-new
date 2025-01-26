f256745eb332d62539bb62b298590a6ff908e82f
Sugaaaa <s10270022@connect.np.edu.sg>
2025-01-26 21:45:41 +0800

    Implemented Terminal class with the required attributes and methods (needs checking/edit)

--- Assignment_Code/Assignment_Code/Terminal.cs
+++ Assignment_Code/Assignment_Code/Terminal.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace Assignment_Code
 {
-    internal class Terminal
+    class Terminal
     {
+        public string TerminalName { get; set; }
+        public Dictionary<string, Airline> Airlines { get; set; }
+        public Dictionary<string, flight> Flights { get; set; }
+        public Dictionary<string, BoardingGate> BoardingGates { get; set; }
+        public Dictionary<string, double> GateFees { get; set; }
+
+        public Terminal(string terminalName)
+        {
+            TerminalName = terminalName;
+            BoardingGates = new Dictionary<string, BoardingGate>();
+            Airlines = new Dictionary<string, Airline>();
+            Flights = new Dictionary<string, flight>();
+            GateFees = new Dictionary<string, double>();
+        }
+
+
+        public void AddAirline(Airline airline)
+        {
+            Airlines[airline.AirlineCode] = airline;
+            return true;
+        }
+
+
+        public void AddBoardingGate(BoardingGate gate)
+        {
+            BoardingGates[gate.GateName] = gate;
+            return true;
+        }
+
+        public Airline GetAirlineFromFlight(flight flight)
+        {
+            foreach (var airline in Airlines.Values)
+            {
+                if (airline.Flights.ContainsKey(flight.flightNumber))
+                {
+                    return airline;
+                }
+            }
+            return null; // if flight not found
+        }
+
+        public void PrintAirlineFees()
+        {
+            foreach (var airline in Airlines.Values)
+            {
+                Console.WriteLine($"{airline.Name} Fees: {airline.CalculateFees():C}");  // temp formatting * edit when testing program
+            }
+        }
+
+        public override string ToString()    // Wrong implementation, needs to be fixed after check * Unsure of what to return, thus temp random values
+        {
+            return $"Terminal: {TerminalName}, Airlines: {Airlines.Count}, Flights: {Flights.Count}, Gates: {BoardingGates.Count}";
+        }
+
     }
-}
+}   
