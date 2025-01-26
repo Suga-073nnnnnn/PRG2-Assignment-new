ef4d8b9229052acc43cb72bfe628e7cde8f60d93
Sugaaaa <s10270022@connect.np.edu.sg>
2025-01-26 20:29:06 +0800

    Implemented the Class with attributes and methods. (require discussion and checking)

--- Assignment_Code/Assignment_Code/Airline.cs
+++ Assignment_Code/Assignment_Code/Airline.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace Assignment_Code
 {
-    internal class Airline
+    class Airline
     {
-    }
+        public string code { get; set; }
+        public string name { get; set; }
+        
+        public Dictionary<string, Flight> flights { get; set; } = new Dictionary<string, Flight>();
+
+
+        public Airline(string Code, string Name)
+        {
+            code = Code;
+            name = Name;
+        }
+
+        public void AddFlight(Flight flight)
+        {
+            Flights[flight.flightNumber] = flight;
+        }
+
+        public void RemoveFlight(string flightNumber)
+        {
+            Flights.Remove(flightNumber);
+        }
+
+        public double CalculateFees()       // Need to test with Flight classes(Gabriel) / in main to see if calculation works correctly
+        {
+            double totalFees = 0.0;
+            foreach (var flight in Flights.Values)      //Discuss on this part.
+            {
+                totalFees += flight.CalculateFee();
+            }
+            return totalFees;
+
+        }
+
 }
