661964fed3c9a0765f3be163e46e37adee43013e
Sugaaaa <s10270022@connect.np.edu.sg>
2025-01-31 13:54:03 +0800

    Update Terminal Class Minor Changes

--- Assignment_Code/Assignment_Code/Terminal.cs
+++ Assignment_Code/Assignment_Code/Terminal.cs
@@ -52,13 +52,13 @@ namespace S10270022_PRG2Assignment
 
         public Airline GetAirlineFromFlight(Flight flight)
         {
-            if(!Airlines.ContainsKey(flight.FlightNumber))
-            {
+            if(!Airlines.ContainsKey(flight.FlightNumber))              // comment this part and use the below function if it doesnt work
+            {                                                                   //fix not tested
                 return null;
             }
             return Airlines[flight.FlightNumber];
 
-            /* foreach (var airline in Airlines.Values)
+            /* foreach (var airline in Airlines.Values)                     
              {
                  if (airline.Flights.ContainsKey(flight.flightNumber))
                  {
